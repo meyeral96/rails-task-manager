8eade2df4c28a55821aa95a0a5dae2641834263b
andrew meyer <andrew@meyerhome.org>
2022-07-16 14:26:03 +0100

    App CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#index", as: :all_tasks
+
+  # Create task
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+  # Read one task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # update task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
 end
